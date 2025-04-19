0a92a05f41123be0a44b824a801ee967b340ce73
nemuel <quispelopeznemuel@gmail.com>
2025-04-19 00:36:07 -0400

    se añadieron componentes y algunos diseños a activity_main_guia.xml

--- app/src/main/java/com/nemo/proyectoguiatributariapsm400/MainActivityMenu.java
+++ app/src/main/java/com/nemo/proyectoguiatributariapsm400/MainActivityMenu.java
@@ -3,6 +3,7 @@ package com.nemo.proyectoguiatributariapsm400;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivityMenu extends AppCompatActivity {
     public ImageButton imgBotonInicio;
+    public Button botonInicioSecion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class MainActivityMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         this.imgBotonInicio = (ImageButton) findViewById(R.id.imgButtonInicio);
+        this.botonInicioSecion = (Button) findViewById(R.id.buttonInicioSesion);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -34,6 +38,13 @@ public class MainActivityMenu extends AppCompatActivity {
                 Intent intent = new Intent(MainActivityMenu.this, MainActivity.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.zoom_back_in, R.anim.zoom_back_out);
+            }
+        });
+
+        this.botonInicioSecion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivityMenu.this, MainActivityGuia.class));
             }
         });
     }
